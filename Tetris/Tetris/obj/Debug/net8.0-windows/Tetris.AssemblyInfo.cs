e4e1f8479050469afe8b0328af34811525be2446
JannathaGitHubVision <vjannatha@gmail.com>
2024-07-01 11:04:23 +0200

    committing the OBlock code

--- Tetris/Tetris/obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
+++ Tetris/Tetris/obj/Debug/net8.0-windows/Tetris.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75844be9efbc13eb0ffaba348059d85aa8b084ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e89bd86dafe91fa63cf9c004492ba16aea925d10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tetris")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
